f8cec4feb061ab388061d56770d227d465cb23b9
Giovanna Andrade Santos <gioandrade@Giovannas-MacBook-Pro.local>
2024-01-23 15:34:47 -0400

    logs middleware

--- express/backend/src/index.ts
+++ express/backend/src/index.ts
@@ -1,17 +1,41 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import validateEnv from './utils/validateEnv';
+import { readFile, writeFile } from 'fs/promises';
 
 dotenv.config();
 validateEnv();
 
 const app = express();
 const PORT = process.env.PORT || 3333;
+const mode = process.argv[2];
+const LOGS = process.env.LOGS;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World !');
+let text: string;
+
+app.use(async (req: Request, res: Response, next: NextFunction) => {
+  console.log(`Requisição ${req.method} ${req.url} mode: ${mode}\n`);
+  text = await (await readFile(`${LOGS}/logs.txt`)).toString();
+  next();
+});
+
+app.use(async (req, res, next: NextFunction) => {
+  if (mode === 'simples') {
+    const date: string = new Date().toISOString();
+    text += `hora de acesso: ${date}, url: ${req.url}, método: ${req.method}\n`;
+    writeFile(`${LOGS}/logs.txt`, text);
+  } else if (mode === 'completo') {
+    const date: string = new Date().toISOString();
+    text = `\nhora de acesso: ${date}, url: ${req.url}, método: ${req.method}, tipo HTTP: ${req.httpVersion}, User-Agent: ${req.get('User-Agent')}\n`;
+    writeFile(`${LOGS}/logs.txt`, text);
+  }
+  next();
+});
+
+app.use('/', (req, res) => {
+  res.send('Hello world!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Aplicação rodando na porta ${PORT}.`);
+  console.log(`Express app iniciada na porta ${PORT}.`);
 });
