48b04d85c6721835b147205c8ff36bfe940d1d7a
Giovanna Andrade Santos <gioandrade@Giovannas-MacBook-Pro.local>
2024-01-23 11:18:20 -0400

    eslint and prettier

--- express/backend/src/index.ts
+++ express/backend/src/index.ts
@@ -1,17 +1,17 @@
-import express, { Request, Response } from "express"
-import dotenv from "dotenv"
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
 import validateEnv from './utils/validateEnv';
 
-dotenv.config()
-validateEnv()
+dotenv.config();
+validateEnv();
 
-const app = express()
-const PORT = process.env.PORT || 3333
+const app = express();
+const PORT = process.env.PORT || 3333;
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Hello World !")
-})
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World !');
+});
 
 app.listen(PORT, () => {
-    console.log(`Aplicação rodando na porta ${PORT}.`)
-})
+  console.log(`Aplicação rodando na porta ${PORT}.`);
+});
