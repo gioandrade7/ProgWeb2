0e3ab925f13a1d9508e9c8d69514e6ab38b5d99e
Giovanna Andrade Santos <gioandrade@Giovannas-MacBook-Pro.local>
2024-01-23 17:26:32 -0400

    lorem backend

--- express/backend/src/index.ts
+++ express/backend/src/index.ts
@@ -1,40 +1,19 @@
-import express, { NextFunction, Request, Response } from 'express';
+import express from 'express';
 import dotenv from 'dotenv';
 import validateEnv from './utils/validateEnv';
-import { readFile, writeFile } from 'fs/promises';
+import morgan from 'morgan';
+import router from './router/router';
 
 dotenv.config();
 validateEnv();
 
 const app = express();
 const PORT = process.env.PORT || 3333;
-const mode = process.argv[2];
-const LOGS = process.env.LOGS;
 
-let text: string;
+app.use(morgan('combined'));
+app.use(router);
 
-app.use(async (req: Request, res: Response, next: NextFunction) => {
-  console.log(`Requisição ${req.method} ${req.url} mode: ${mode}\n`);
-  text = await (await readFile(`${LOGS}/logs.txt`)).toString();
-  next();
-});
-
-app.use(async (req, res, next: NextFunction) => {
-  if (mode === 'simples') {
-    const date: string = new Date().toISOString();
-    text += `hora de acesso: ${date}, url: ${req.url}, método: ${req.method}\n`;
-    writeFile(`${LOGS}/logs.txt`, text);
-  } else if (mode === 'completo') {
-    const date: string = new Date().toISOString();
-    text = `\nhora de acesso: ${date}, url: ${req.url}, método: ${req.method}, tipo HTTP: ${req.httpVersion}, User-Agent: ${req.get('User-Agent')}\n`;
-    writeFile(`${LOGS}/logs.txt`, text);
-  }
-  next();
-});
-
-app.use('/', (req, res) => {
-  res.send('Hello world!');
-});
+app.use('/img', express.static(`${__dirname}/../public/img`));
 
 app.listen(PORT, () => {
   console.log(`Express app iniciada na porta ${PORT}.`);
---
+++ express/backend/src/router/router.ts
@@ -0,0 +1,34 @@
+import { Router } from 'express';
+import { LoremIpsum } from 'lorem-ipsum';
+
+const router = Router();
+
+const lorem = new LoremIpsum({
+  sentencesPerParagraph: {
+    max: 8,
+    min: 4,
+  },
+  wordsPerSentence: {
+    max: 16,
+    min: 4,
+  },
+});
+
+router.get('/', (req, res) => {
+  res.send('Hello world!');
+});
+
+router.get('/bemvindo/:nome', (req, res) => {
+  const nome = req.params.nome;
+  res.send(`Bem-vindo(a) ${nome}`);
+});
+
+router.get('/lorem/:qtd', (req, res) => {
+  res.send(
+    lorem
+      .generateParagraphs(parseInt(req.params.qtd))
+      .replace(/\n/g, '<br><br>\n'),
+  );
+});
+
+export default router;
